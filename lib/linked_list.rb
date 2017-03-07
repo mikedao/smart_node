de050e69d16a02a2e2379e2d683e74a275c4f69f
Michael Dao <michael.dao@gmail.com>
2017-03-07 14:24:31 -0700

    Rewrote everything cause I no longer want a list to start with a node automatically

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,10 +1,11 @@
+require './lib/node'
 class LinkedList
   attr_reader :head
 
-  def initialize(data)
-    @head = Node.new(data)
+  def initialize
+    @head = nil
   end
-  
+
   def push(data)
     if @head.nil?
       @head = Node.new(data)
@@ -12,5 +13,4 @@ class LinkedList
       @head.push(data)
     end
   end
-
 end
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -3,26 +3,37 @@ require 'minitest/pride'
 require './lib/linked_list'
 
 class LinkedListTest < Minitest::Test
-  def test_list_starts_with_a_node_at_head
-    l = LinkedList.new("Hermione")
-  
-    assert_equal "Hermione", l.head.data
-    assert_equal Node, l.head.class
-  end  
+  def test_list_starts_empty
+    l = LinkedList.new
 
-  def test_list_can_push_items_on
-    l = LinkedList.new("Hermione")
-
-    refute l.head.next_node
-
-    l.push("Luna")
-
-    assert_equal "Luna", l.head.next_node.data
-    assert_equal Node, l.head.next_node.class
-
-    l.push("Neville")
-
-    assert_equal "Neville", l.head.next_node.next_node.data
-    assert_equal Node, l.head.next_node.next_node.class
+    refute l.head
   end
+
+  def test_list_can_push_one_item_and_its_a_node
+    l = LinkedList.new
+    
+    l.push("Harry")
+
+    assert_equal "Harry", l.head.data
+    assert_equal Node , l.head.class
+  end
+
+  def test_list_can_push_many_nodes
+    l = LinkedList.new
+    
+    l.push("Harry")
+    l.push("Hermione")
+    l.push("Snape")
+    l.push("McGonagall")
+
+    assert_equal "Harry", l.head.data
+    assert_equal Node , l.head.class
+    assert_equal "Hermione", l.head.next_node.data
+    assert_equal Node , l.head.next_node.class
+    assert_equal "Snape", l.head.next_node.next_node.data
+    assert_equal Node , l.head.next_node.next_node.class
+    assert_equal "McGonagall", l.head.next_node.next_node.next_node.data
+    assert_equal Node , l.head.next_node.next_node.next_node.class
+  end
+
 end
